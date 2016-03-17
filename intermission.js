595f7ed67728bf9bd0ad5600fd8b03fc8879c464
Louis Ritchie <louiscritchie@gmail.com>
2016-03-16 19:01:44 -0700

    Removed redundant methods and variables, reduced setInterval times (prime numbers are ideal for this)

--- intermission.js
+++ intermission.js
@@ -8,11 +8,10 @@
 var times = [];
 /* times array. All times in ms since epoch.
 	element:	represents:
-	0		time at initialization, never updated
+	0		time at initialization + time spent paused
 	1		current time
 	2		time of last pause	
 	3		time of last resume
-	4		sum of all time spent paused, to subtract from times[1]
 */
 var interval, paused, button, div;
 
@@ -22,47 +21,50 @@ div = document.getElementsByClassName("main")[0];
 
 button.addEventListener("click", init);
 
+/* init()
+set initial times, switch button click handlers
+*/
 function init() 
 {
 	times[0] = times[1] = new Date().getTime();
-	times[2] = times[3] = times[4] = 0;
+	times[2] = 0;
 	button.removeEventListener("click", init);
 	button.addEventListener("click", pauseHandler);
 	button.innerHTML = "Pause";
-	interval = setInterval(resume, 67)	
+	interval = setInterval(resume, 37)	
 }
 
+/* pauseHandler()
+either begin or resume & manage times
+*/
 function pauseHandler() 
 {
 	clearInterval(interval);
 	times[1] = new Date().getTime();
 	if (paused) {
-		times[3] = times[1];
-		times[4] += times[3] - times[2];
-		interval = setInterval(resume, 67);
-		button.innerHTML = "Pause";
+		times[0] += times[1] - times[2];
+		interval = setInterval(resume, 37);
 	} else {
 		times[2] = times[1];
-		interval = setInterval(pause, 67);
-		button.innerHTML = "Resume"
 	}
 	paused = !paused;		
+	button.innerHTML = paused ? "Resume" : "Pause"
 }
 
+/* resume()
+call by setInterval() in pauseHandler(), writes the time to HTML.
+*/
 function resume() 
 {
 	times[1] = new Date().getTime();
-	writeTimeToHTML(times[1] - times[4] - times[0])
+	writeTimeToHTML(times[1] - times[0])
 }
 
-function pause() 
-{
-	writeTimeToHTML(times[2] - times[4] - times[0])
-}
-
+/* writeTimeToHTML
+formats the time into seconds:ms.
+*/
 function writeTimeToHTML(t) {
         output = (t/1000).toString().split(".");
         output[1] = output[1] == null ? 0 : output[1];
         div.innerHTML = output[0] + ":" + output[1]
 }
-
